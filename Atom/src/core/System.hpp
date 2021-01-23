dde5c912529c3dce0a44ec37ca4b0e030fd43330
Abhikalp Unakal <abhikalpunakal@outlook.com>
2021-01-22 18:05:16 -0800

    Entity Component System core init + Serde demo with example to render rectangles from files + ChrononManager(FPS) + MultiPlatform ResourceManager + ResourceLoader

---
+++ Atom/src/core/System.hpp
@@ -0,0 +1,25 @@
+#ifndef SYSTEM_HPP
+#define SYSTEM_HPP
+
+#include "Pch.hpp"
+#include "core/Types.hpp"
+//forward declare 
+class Event;
+
+class System {
+public:
+	virtual void init() {
+
+	}
+	virtual void update() {
+
+	}
+	virtual void onEvent(Event& e) {
+
+	}
+public:
+	std::set<EntityID> mEntities;
+	bool mInit = false;
+};
+
+#endif
