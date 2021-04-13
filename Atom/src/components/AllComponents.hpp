ad5804a6130fecfa532ebd55bc9a7a9bfc70a8e1
kishore <77184969+KishoreKB2711@users.noreply.github.com>
2021-04-13 23:33:20 +0530

    Intergrated Moving on Trigger and Floating to current main

--- Atom/src/components/AllComponents.hpp
+++ Atom/src/components/AllComponents.hpp
@@ -22,5 +22,6 @@
 #include "components/PushUpComponent.hpp"
 #include "components/ParticleComponent.hpp"
 #include "components/AnimationComponent.hpp"
+#include "components/MoveToComponent.hpp"
 
 #endif 
